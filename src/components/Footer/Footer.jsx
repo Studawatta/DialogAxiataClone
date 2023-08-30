3bcace8c94e6abf84a1a0bb50bb14384e0f1b279
Studawatta <st.udawatte@gmail.com>
2023-08-30 23:54:08 +0530

    footer completed

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,55 @@
+import "./Footer.scss";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="topContainer">
+          <div>
+            <h1>Explore & Shop</h1>
+            <span>Mobile</span>
+            <span>Fixed Line</span>
+            <span>Broadband</span>
+            <span>Television</span>
+            <span>International</span>
+            <span>Loyalty</span>
+            <span>Careers</span>
+          </div>
+
+          <div>
+            <h1>Partners & Other Services</h1>
+            <span>eZ Cash</span>
+            <span>Genie</span>
+            <span>IdeaMart</span>
+            <span>Dialog Finance</span>
+            <span>Start Points</span>
+            <span>View all</span>
+          </div>
+
+          <div>
+            <h1>Support</h1>
+            <span>How do I obtain a Dialog Mobile connection</span>
+            <span>How to confirm ownership for my mobile connection</span>
+            <span>How do I check my Dialog TV Bill?</span>
+            <span>What do i get from the 4G Mobile LTE service?</span>
+            <span>Run out of credit? Get our auto loan facility</span>
+            <span>View All FAQs</span>
+          </div>
+
+          <div>
+            <h1>Contact Us</h1>
+            <span>service@dialog.lk</span>
+            <span>Skype</span>
+            <span>Contact Info</span>
+            <span>Seft-Help Channels</span>
+
+            <h1>Coverage Map</h1>
+            <span>View Network Coverage</span>
+          </div>
+        </div>
+      </div>
+      <div className="bottom"></div>
+    </div>
+  );
+};
+
+export default Footer;
